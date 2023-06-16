d43a28c23a7e35b9c44605a8e22bd1ad2ebe0d0a
anderson.hemkmaier <anderson.hemkmaier@BUE0205D030.alunos.sc.senac.br>
2023-06-16 16:44:33 -0300

    Segundo Commit

--- incregacaoGitEclipse/src/GitTeste/TesteIncregacaoGitHub.java
+++ incregacaoGitEclipse/src/GitTeste/TesteIncregacaoGitHub.java
@@ -4,6 +4,8 @@ public class TesteIncregacaoGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Teste GitHub");
+		System.out.println("Fazendo primeira alteração");
 
 	}
 
